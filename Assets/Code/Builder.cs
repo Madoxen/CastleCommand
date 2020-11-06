5c950a0c3e0f25af95b6d10991001bcc3cf4e517
Paweł Chłąd <pchlad5@gmail.com>
2020-11-06 17:41:22 +0100

    Ghost now moves on terrain when player is moving its mouse

--- Assets/Code/Builder.cs
+++ Assets/Code/Builder.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //Responsible for positioning and instantiating buildings
 //Also shows building ghost if a building is chosen
@@ -9,8 +10,7 @@ public class Builder : MonoBehaviour
     [SerializeField]
     GameObject currentBuildingPrefab;
     [SerializeField]
-    GameObject buildingGhostObject; 
-
+    GameObject buildingGhostObject;
 
 
     public GameObject CurrentBuildingPrefab
@@ -23,18 +23,19 @@ public class Builder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+
+     
+
+
+
     }
 
-
-
+    
     //TODO: research property serialization and possible event call on assignment
     public void selectPrefab(GameObject buildingPrefab)
     {
--- Assets/Code/BuildingGhost.cs
+++ Assets/Code/BuildingGhost.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 namespace Assets
 {
@@ -32,32 +33,62 @@ namespace Assets
             set
             {
                 collisionCount = value;
-                
                 IsValid = (collisionCount == 0);
             }
         }
 
         [SerializeField]
         Material ghostMat;
-
+        MasterInput input;
         MeshRenderer Renderer;
+        int mask;
 
         private void Awake()
         {
+            mask = 1 << 8;
+            input = new MasterInput();
             Renderer = GetComponent<MeshRenderer>();
             Renderer.material = ghostMat;
+            input.Player.Mouse.performed += GhostMoved_performed;
+        }
+
+        private void GhostMoved_performed(InputAction.CallbackContext context)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            //Raycast against the terrain
+            //256 -> 10000000 8th bit
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, mask))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                this.transform.position = hit.point;
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin,ray.direction,Color.black);
+            }
+
         }
 
 
+        private void OnEnable()
+        {
+            input.Player.Mouse.Enable();
+        }
+
+        private void OnDisable()
+        {
+            input.Player.Mouse.Disable();
+        }
+
         void OnTriggerEnter(Collider other)
         {
-            if (gameObject.layer == LayerMask.NameToLayer("Buildings"))
+            if (other.gameObject.layer == LayerMask.NameToLayer("Buildings"))
                 CollisionCount++;
         }
 
         void OnTriggerExit(Collider other)
         {
-            if (gameObject.layer == LayerMask.NameToLayer("Buildings"))
+            if (other.gameObject.layer == LayerMask.NameToLayer("Buildings"))
                 CollisionCount--;
         }
     }
