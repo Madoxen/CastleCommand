555e266424ee6d9bcae6fe3dae44b3f9d8570679
Paweł Chłąd <pchlad5@gmail.com>
2020-11-12 11:41:51 +0100

    Merged #2
    Squashed commit of the following:
    
    commit 7e64728f07dc10176bf17c292d1439da8c69d805
    Author: Paweł Chłąd <pchlad5@gmail.com>
    Date:   Thu Nov 12 11:40:04 2020 +0100
    
        Added basic enemy melee attack

---
+++ Assets/Code/EnemyBasicMeleeAttack.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SphereCollider))]
+public class EnemyBasicMeleeAttack : MonoBehaviour
+{
+    [SerializeField]
+    private int damage;
+    public int Damage
+    {
+        get { return damage; }
+    }
+
+    [SerializeField]
+    private float attackSpeed;
+    public float AttackSpeed
+    {
+        get { return attackSpeed; }
+        set { attackSpeed = value; }
+    }
+
+    [SerializeField]
+    private GameObject currentAttackTarget = null;
+    public GameObject CurrentAttackTarget
+    {
+        get { return currentAttackTarget; }
+        set { currentAttackTarget = value; }
+    }
+
+    private float currentAttackCooldown = 0f;
+
+    private void FixedUpdate()
+    {
+        if (CurrentAttackTarget != null) //This might be performance taxing since we can have dozens of enemies on screen
+        {
+            currentAttackCooldown -= Time.fixedDeltaTime;
+            if (currentAttackCooldown <= 0)
+            {
+                Attack();
+                currentAttackCooldown = AttackSpeed;
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        CurrentAttackTarget.GetComponent<HealthComponent>().CurrentHealth -= Damage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Building>() && other.gameObject.GetComponent<HealthComponent>()) //This might be performance taxing
+        {
+            if (currentAttackTarget == null)
+            {
+                currentAttackTarget = other.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == currentAttackTarget) 
+        {
+            currentAttackTarget = null;
+        }
+    }
+
+}
