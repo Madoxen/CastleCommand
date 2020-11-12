7e64728f07dc10176bf17c292d1439da8c69d805
Paweł Chłąd <pchlad5@gmail.com>
2020-11-12 11:40:04 +0100

    Added basic enemy melee attack

--- Assets/Code/HealthComponent.cs
+++ Assets/Code/HealthComponent.cs
@@ -4,21 +4,61 @@ using System;
 
 public class HealthComponent : MonoBehaviour
 {
-    private int currentHealth;
+    [SerializeField]
+    private int currentHealth = 1;
     public int CurrentHealth
     {
         get { return currentHealth; }
-        set { currentHealth = value; OnHealthChanged(); }
+        set
+        {
+            currentHealth = value;
+            if (currentHealth > maxHealth)
+                currentHealth = maxHealth;
+
+            OnHealthChanged();
+        }
+    }
+
+    [SerializeField]
+    private int maxHealth = 1;
+    public int MaxHealth
+    {
+        get { return maxHealth; }
+        set
+        {
+            maxHealth = value;
+            if (currentHealth > maxHealth)
+                currentHealth = maxHealth;
+
+            OnHealthChanged();
+        }
+    }
+
+    private void Awake()
+    {
+        CurrentHealth = MaxHealth;
     }
 
     public delegate void HealthChangedHandler(object sender, int currentHealth);
     public event HealthChangedHandler HealthChangedEvent;
+    public event HealthChangedHandler MaxHealthChangedEvent;
 
     void OnHealthChanged()
     {
         //Notify subscribers about change of health
-        HealthChangedEvent(this, currentHealth);
+        HealthChangedEvent?.Invoke(this, currentHealth);
         if (CurrentHealth <= 0)
             Destroy(this.gameObject);
+
+
+    }
+
+    void OnMaxHealthChanged()
+    {
+        //Notify subscribers about change of max health
+        MaxHealthChangedEvent?.Invoke(this, currentHealth);
+
+
+
     }
 }
