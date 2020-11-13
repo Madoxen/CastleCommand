0e0fb025b73cc187b740a2149ce5f5c67405b1a3
MellerB <mellerbartomiej@gmail.com>
2020-11-13 18:58:19 +0100

    camera controls (drag + zoom) and bounds done

--- Assets/Code/CameraControls.cs
+++ Assets/Code/CameraControls.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
-    public float speed = 1;
+    public Vector2 center = new Vector2(90f,100f);
+    public float maxDistanceFromCenter = 10f;
+    public float maxHeight = 8f;
+    public float minHeight = 2f;
 
+    public float speed = 2.5f;
+    public float zoomingSpeed = 0.5f;
+    public bool moveAllowed = false;
 
     MasterInput input;
 
@@ -13,13 +19,35 @@ public class CameraControls : MonoBehaviour
     void Awake()
     {
         input = new MasterInput();
+        input.Camera.MoveAllowed.started += MoveAllowed_Started;
+        input.Camera.MoveAllowed.canceled += MoveAllowed_Canceled;
     }
 
 
     private void Update()
     {
-        Vector2 axis = input.Camera.Move.ReadValue<Vector2>();
-        transform.position = transform.position + new Vector3(axis.x * speed, 0, axis.y * speed);
+        Vector2 axis = Camera.main.ScreenToViewportPoint(input.Camera.Move.ReadValue<Vector2>());
+        float zoom = input.Camera.Zoom.ReadValue<float>();
+        Vector3 camPos = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+
+        Debug.Log(zoom);
+
+        if (moveAllowed) camPos = transform.position - speed * transform.position.y * (Quaternion.Euler(0f,transform.rotation.eulerAngles.y,0f) * (new Vector3(axis.x, 0, axis.y)));
+        camPos += (Quaternion.Euler(-transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f) * (new Vector3(0, zoom * zoomingSpeed, 0)));
+
+
+        transform.position = bound(camPos);
+    }
+
+    void MoveAllowed_Started(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        moveAllowed = true;
+    }
+
+
+    void MoveAllowed_Canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        moveAllowed = false;
     }
 
 
@@ -28,11 +56,34 @@ public class CameraControls : MonoBehaviour
     private void OnEnable()
     {
         input.Camera.Move.Enable();
+        input.Camera.MoveAllowed.Enable();
+        input.Camera.Zoom.Enable();
     }
 
     private void OnDisable()
     {
         input.Camera.Move.Disable();
+        input.Camera.MoveAllowed.Disable();
+        input.Camera.Zoom.Disable();
     }
 
+
+    private Vector3 bound(Vector3 position)
+    {
+        float x = position.x;
+        float y = position.y;
+        float z = position.z;
+
+        if (x > center.x+maxDistanceFromCenter) x = center.x + maxDistanceFromCenter;
+        else if (x < center.x - maxDistanceFromCenter) x = center.x - maxDistanceFromCenter;
+
+        if (y > maxHeight) y = maxHeight;
+        else if (y < minHeight) y = minHeight;
+
+        if (z > center.y + maxDistanceFromCenter) z = center.y + maxDistanceFromCenter;
+        else if (z < center.y - maxDistanceFromCenter) z = center.y - maxDistanceFromCenter;
+
+        return new Vector3(x, y, z);
+
+    }
 }
