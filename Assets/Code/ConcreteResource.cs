b45813a63149cb8847d672c8ad36ee038dd7fb31
Paweł Chłąd <pchlad5@gmail.com>
2020-11-15 16:18:21 +0100

    Added resource manager class and simple UI generation

---
+++ Assets/Code/ConcreteResource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class ConcreteResource
+{
+    private int amount;
+    public int Amount
+    {
+        get { return amount; }
+        set
+        {
+            amount = value;
+
+
+            if (amount > maxAmount)
+                amount = maxAmount;
+
+
+            AmountChanged?.Invoke(Amount);
+        }
+    }
+
+
+    public int maxAmount;
+    public int MaxAmount
+    {
+        get { return maxAmount; }
+        set
+        {
+            maxAmount = value;
+
+            if (amount > maxAmount)
+                amount = maxAmount;
+
+            MaxAmountChanged?.Invoke(MaxAmount);
+        }
+    }
+
+
+    public StrategicResource resource;
+
+    public ConcreteResource(StrategicResource r, int maxAmount)
+    {
+        resource = r;
+        this.maxAmount = maxAmount;
+    }
+
+
+    public event Action<int> AmountChanged;
+    public event Action<int> MaxAmountChanged;
+}
---
+++ Assets/Code/ResourceGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code
+{
+    public class ResourceGenerator : MonoBehaviour
+    {
+        public StrategicResource generatedResource;
+        public float speed;
+        public int amount;
+
+
+        private ConcreteResource r;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine(GenerateResource());
+            r = PlayerResources.Instance.Resources.Find(x => x.resource == generatedResource);
+        }
+
+
+        IEnumerator GenerateResource()
+        {
+            r.Amount += amount;
+            yield return new WaitForSeconds(speed);
+        }
+    }
+}
