86b0204fb6c649e91f1b7f36be0748ae97e9ca86
Quackeee <krzysztofklak7@gmail.com>
2020-11-18 23:04:16 +0100

    move controls to MasterInput

--- Assets/Code/Destroyer.cs
+++ Assets/Code/Destroyer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEditor;
-
+using System;
 
 public class Destroyer : MonoBehaviour
 {
@@ -26,31 +26,33 @@ public class Destroyer : MonoBehaviour
     GameObject lastHit;
     Material lastMaterial;
     Material destroyShader;
-    // Start is called before the first frame update
-    void Start()
+
+    MasterInput input;
+
+    private void Awake()
     {
+        input = new MasterInput();
+
+        input.Destroyer.ConfirmDelete.performed += _ => OnConfirmDelete();
+        input.Destroyer.CancelDelete.performed += _ => OnCancelDelete();
+        input.Destroyer.MouseMove.performed += _ => OnMouseMove();
+
         camera = Camera.main;
         buildingsLayer = LayerMask.GetMask("Buildings");
         destroyShader = AssetDatabase.LoadAssetAtPath<Material>("Assets/Materials/Purple.2");
     }
-    
-    void OnEnable()
-    {
-        builder.CurrentBuildingPrefab = null; //hope that's the right way to turn the builder off
-    }
-    // Update is called once per frame
-    void Update()
+
+    private void OnMouseMove()
     {
         Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsLayer))
         {
-            //TODO: show that the building is selected somehow like paint it red or sth
+            //building gets collored to show it is aimed at
             var hitObject = hit.collider.gameObject;
 
-            if (hitObject != lastHit) {
-
+            if (hitObject != lastHit)
+            {
                 if (lastHit != null)
                     lastHit.GetComponent<Renderer>().material = lastMaterial;
 
@@ -60,12 +62,6 @@ public class Destroyer : MonoBehaviour
 
                 hitObject.GetComponent<Renderer>().material = destroyShader;
             }
-
-
-            if (Mouse.current.leftButton.wasReleasedThisFrame)
-            {
-                Destroy(hit.collider.gameObject);
-            }
         }
         else if (lastHit != null)
         {
@@ -73,5 +69,30 @@ public class Destroyer : MonoBehaviour
             lastHit = null;
             lastMaterial = null;
         }
+    }
+
+    private void OnCancelDelete()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnConfirmDelete()
+    {
+        if (lastHit != null)
+        {
+            Destroy(lastHit);
+            lastMaterial = null;
+        }
+    }
+
+    private void OnEnable()
+    {
+        builder.CurrentBuildingPrefab = null; //hope that's the right way to turn the builder off
+        input.Destroyer.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Destroyer.Disable();
     }
 }
