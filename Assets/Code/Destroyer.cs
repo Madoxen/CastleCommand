52337abc0d5a482485af8eafaa7f3081aca5df8b
Quackeee <krzysztofklak7@gmail.com>
2020-11-18 15:15:54 +0100

    add destroyer button and object

---
+++ Assets/Code/Destroyer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Destroyer : MonoBehaviour
+{
+    Camera camera;
+    RaycastHit hit;
+    int buildingsLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        buildingsLayer = LayerMask.GetMask("Buildings");
+        Debug.Log(buildingsLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsLayer))
+        {
+            //TODO: show that the building is selected somehow like paint it red or sth
+
+            if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
