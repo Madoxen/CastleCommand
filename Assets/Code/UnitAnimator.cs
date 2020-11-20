40da4b808d3560ba751a3a12f6cf5d430615fab5
Paweł Chłąd <pchlad5@gmail.com>
2020-11-20 20:01:46 +0100

    Basic skeleton anims in

---
+++ Assets/Code/UnitAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+using System;
+
+
+public class UnitAnimator : MonoBehaviour
+{
+    Animator animator;
+    IDamageDealer dd;
+
+    Action<object> onAttack;
+
+    // Use this for initialization
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        dd = GetComponent<IDamageDealer>();
+
+        if (dd != null)
+        {
+            onAttack = (sender) => animator.SetTrigger("Attacked");
+            dd.Attacked += onAttack;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        dd.Attacked -= onAttack;
+    }
+}
