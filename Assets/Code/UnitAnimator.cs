19ef3b8b86568d31c92b6d7555529982bbcbf943
Paweł Chłąd <pchlad5@gmail.com>
2020-11-20 20:36:25 +0100

    More transition stuff

--- Assets/Code/UnitAnimator.cs
+++ Assets/Code/UnitAnimator.cs
@@ -8,20 +8,30 @@ public class UnitAnimator : MonoBehaviour
 {
     Animator animator;
     IDamageDealer dd;
+    NavMeshAgent navAgent;
 
     Action<object> onAttack;
 
+    
     // Use this for initialization
     void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         dd = GetComponent<IDamageDealer>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        
 
         if (dd != null)
         {
             onAttack = (sender) => animator.SetTrigger("Attacked");
             dd.Attacked += onAttack;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetFloat("MovementSpeed%", navAgent.velocity.magnitude / navAgent.speed);
     }
 
     private void OnDestroy()
