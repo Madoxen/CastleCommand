790fc1cbd7f44387bb26fdffeded1ff7c4760893
MellerB <mellerbartomiej@gmail.com>
2021-01-27 20:55:25 +0100

    make vawe timer

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
 
+
+
+
+public static class EnumerableExtension
+{
+    public static T PickRandom<T>(this IEnumerable<T> source)
+    {
+        return source.PickRandom(1).Single();
+    }
+
+    public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+    {
+        return source.Shuffle().Take(count);
+    }
+
+    public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+    {
+        return source.OrderBy(x => Guid.NewGuid());
+    }
+}
 
 
 public class EnemySpawner : MonoBehaviour
 {
+    private static PlayerResources instance = null;
+    public static PlayerResources Instance
+    {
+        get { return instance; }
+    }
+
+
+
     [SerializeField]
     private ScenarioSC scenarioSC;
     public ScenarioSC ScenarioSC
@@ -21,11 +52,16 @@ public class EnemySpawner : MonoBehaviour
         get { return enemyListSC; }
     }
 
+    public List<GameObject> SpawnerList;
+
+    public GameObject TimerLabel;
+
+    private float timeLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnEnemy());
+         StartCoroutine(SpawnEnemy());
     }
 
     // Update is called once per frame
@@ -34,11 +70,19 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-    void Spawn(GameObject chosenPrefab, int n)
+    private void FixedUpdate() {
+        timeLeft -= Time.deltaTime;
+        TimeSpan t = TimeSpan.FromSeconds(timeLeft);
+        TimerLabel.GetComponent<Text>().text = string.Format("{0:D2}:{1:D2}",
+        t.Minutes,
+        t.Seconds);
+    }
+
+    void Spawn(GameObject chosenPrefab, int n, Transform _transform)
     {
         for (int i = 0; i < n; i++)
         {
-            Instantiate(chosenPrefab, transform.position, Quaternion.identity);
+            Instantiate(chosenPrefab, _transform.position, Quaternion.identity);
         }
         
     }
@@ -47,11 +91,16 @@ public class EnemySpawner : MonoBehaviour
     {
         foreach (var wave in scenarioSC.Waves)
         {
-            var enemies = EnemyKnapsack.MakeVawe(wave.Budget,enemyListSC.Pricing);
+            Dictionary<GameObject, int> enemies = EnemyKnapsack.MakeVawe(wave.Budget,enemyListSC.Pricing);
             foreach (var enemy in enemies)
             {
-                Spawn(enemy.Key, enemy.Value);
+                Spawn(enemy.Key,
+                 enemy.Value,
+                 SpawnerList.PickRandom().transform);
             }
+
+            timeLeft=wave.Time;
+
             yield return new WaitForSeconds(wave.Time);
         }
     }
---
+++ Assets/Code/SCs/SpawnerListSC.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CreateSpawnerList", order = 1)]
+public class SpawnerListSC: ScriptableObject
+{
+    public List<GameObject> data = new List<GameObject>();
+}
