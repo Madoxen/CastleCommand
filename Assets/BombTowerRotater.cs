09e3c3649a07ed1905852722f6fde37a8d99ae82
Madoxen <pchlad5@gmail.com>
2021-01-27 17:36:52 +0100

    Fix for UI

--- Assets/BombTowerRotater.cs
+++ Assets/BombTowerRotater.cs
@@ -19,8 +19,11 @@ public class BombTowerRotater : MonoBehaviour
     private void FixedUpdate()
     {
         //Rotate towards last velocity, but only around Y (up, green axis)
-        Vector3 euler = Quaternion.LookRotation(dealer.lastVelocity, transform.up).eulerAngles;
-        transform.localRotation = Quaternion.Euler(new Vector3(0, euler.y, 0));
-        cannon.transform.localRotation = Quaternion.Euler(new Vector3(euler.x, 0, 0));
+        if (dealer.lastVelocity != Vector3.zero)
+        {
+            Vector3 euler = Quaternion.LookRotation(dealer.lastVelocity, transform.up).eulerAngles;
+            transform.localRotation = Quaternion.Euler(new Vector3(0, euler.y, 0));
+            cannon.transform.localRotation = Quaternion.Euler(new Vector3(euler.x, 0, 0));
+        }
     }
 }
