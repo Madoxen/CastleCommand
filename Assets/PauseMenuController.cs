0993d702d6c298a3c1403de626bbf61008d378a4
Quackeee <krzysztofklak7@gmail.com>
2021-02-17 16:26:48 +0100

    Hook volume sliders up to mixer.

---
+++ Assets/PauseMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer mixer;
+    [SerializeField]
+    private MainSceneController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Resume()
+    {
+        controller.OnPause();
+    }
+
+    public void UpdateMasterVolume(float value)
+    {
+        mixer.SetFloat("Master", value);
+    }
+
+    public void UpdateMusicVolume(float value)
+    {
+        mixer.SetFloat("Music", value);
+    }
+    public void UpdateEffectsVolume(float value)
+    {
+        mixer.SetFloat("Effects", value);
+    }
+
+    public void Quit()
+    {
+
+    }
+}
